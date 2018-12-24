f837877ae8fb3cec5345538b875d8d417d131bbe
Diep Pham <me@favadi.com>
2018-12-24 14:02:29 +0700

    improves Application type comment

--- app-names/common/types.go
+++ app-names/common/types.go
@@ -2,7 +2,7 @@ package common
 
 import "github.com/ethereum/go-ethereum/common"
 
-// Application an app
+// Application represents a third party application powered by KyberNetwork.
 type Application struct {
 	ID        int64            `json:"id,omitempty"`
 	Name      string           `json:"name" binding:"required"`
